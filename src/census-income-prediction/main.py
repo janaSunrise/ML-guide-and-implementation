ae735d2cf53833649922692437382c73dbf82c4f
janaSunrise <warriordefenderz@gmail.com>
2020-12-07 17:05:18 +0530

    fix flake8 errors

--- src/census-income-prediction/main.py
+++ src/census-income-prediction/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 
 column_names = [
