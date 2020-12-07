342bcf6ca09e3646b9997260452172b96b0aee3e
janaSunrise <warriordefenderz@gmail.com>
2020-12-07 11:31:24 +0530

    Add graph visualization to Mall customer prediction

--- src/mall-customers-prediction/main.py
+++ src/mall-customers-prediction/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeRegressor
@@ -19,5 +20,16 @@ model.fit(X, y)
 
 y_pred = model.predict(X_test)
 
-# Print the stats
+# Print the score
 print(f"Score: {model.score(X_test, y_test)}")
+
+# Plot Test vs Pred
+plot_df = pd.DataFrame({
+    'Actual': y_test.flatten(),
+    'Predicted': y_pred.flatten()
+})
+
+plot_df.head(25).plot(kind='bar', figsize=(16, 10))
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
