db0f58075ef54c4c41a813df5f4744e6fd42a796
Rohith04MVK <67266075+Rohith04MVK@users.noreply.github.com>
2020-12-08 09:27:16 +0530

    Added Student score

---
+++ src/Student-Score/main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Lasso
+
+df = pd.read_csv("student-mat.csv", sep=';')\
+    .apply(LabelEncoder().fit_transform)
+# Convert the whole DF in integer based values.
+
+X = df.drop("G3", axis=1).values
+y = df["G3"].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.2,
+                                                    random_state=42)
+
+# Defining a model
+model = Lasso(alpha=1)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+# Print the stats
+print(f"Accuracy: {model.score(X_test, y_test)}")
