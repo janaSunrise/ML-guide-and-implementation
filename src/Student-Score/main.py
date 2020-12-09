c42abab7fbcbbe6635029223b632ecbe0b32dc84
Rohith04MVK <67266075+Rohith04MVK@users.noreply.github.com>
2020-12-09 07:41:49 +0530

    Apply suggestions from code review
    Co-authored-by: Warrior Defend <warriordefenderz@gmail.com>

--- src/Student-Score/main.py
+++ src/Student-Score/main.py
@@ -1,22 +1,18 @@
 import pandas as pd
-
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Lasso
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv("student-mat.csv", sep=';')\
-    .apply(LabelEncoder().fit_transform)
-# Convert the whole DF in integer based values.
+       .apply(LabelEncoder().fit_transform)  # Convert the whole DF in integer based values.
 
 X = df.drop("G3", axis=1).values
 y = df["G3"].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                    test_size=0.2,
-                                                    random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Defining a model
-model = Lasso(alpha=1)
+model = Lasso()
 
 model.fit(X_train, y_train)
 
