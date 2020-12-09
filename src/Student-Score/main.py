1827151a3cc91164daa37a4b31e04c4c29cc3236
Rohith04MVK <67266075+Rohith04MVK@users.noreply.github.com>
2020-12-09 08:32:04 +0530

    Added plots

--- src/Student-Score/main.py
+++ src/Student-Score/main.py
@@ -20,3 +20,15 @@ y_pred = model.predict(X_test)
 
 # Print the stats
 print(f"Accuracy: {model.score(X_test, y_test)}")
+
+# Plotting the data
+plot_df = pd.DataFrame({
+    'Actual': y_test.flatten(),
+    'Predicted': y_pred.flatten()
+})
+
+plot_df.head(25).plot(kind='bar', figsize=(16, 10))
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
+
