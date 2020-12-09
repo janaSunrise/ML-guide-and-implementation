d85d7da7ff5f9000a7b772de3c1ff7f03f70b5ab
Rohith04MVK <67266075+Rohith04MVK@users.noreply.github.com>
2020-12-09 08:35:27 +0530

    Update main.py

--- src/Student-Score/main.py
+++ src/Student-Score/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.linear_model import Lasso
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
