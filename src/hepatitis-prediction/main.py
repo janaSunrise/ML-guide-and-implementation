61ed9b601eb34ccd8ed08bf802196151654941a1
Rohith04MVK <67266075+Rohith04MVK@users.noreply.github.com>
2020-12-09 17:02:50 +0530

    Update main.py

--- src/hepatitis-prediction/main.py
+++ src/hepatitis-prediction/main.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+
 column_names = [
     "class", "age", "sex", "steroid", "antivirals", "fatigue", "malaise"
     "anorexia", "liver_big", "liver_firm", "spleen_palpable", "spiders",
@@ -14,5 +18,25 @@ df = pd.read_csv("hepatitis.csv", names=column_names)\
     .drop_duplicates()
 
 df = df.fillna(df.mean().to_dict())
+df["class"] = df["class"].astype("bool")
 
-print(df)
+df1 = pd.get_dummies(df, drop_first=True)
+
+
+df1["bilirubin"] = np.abs((df1["bilirubin"]-df1["bilirubin"].mean())/(df1["bilirubin"].std()))
+df1["albumin"] = np.abs((df1["albumin"]-df1["albumin"].mean())/(df1["albumin"].std()))
+
+X = df1.drop(columns=["class"])
+y = df1["class"].copy()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+print("\n shape of train split: ")
+print(X_train.shape, y_train.shape)
+print("\n shape of train split: ")
+print(X_test.shape, y_test.shape)
+
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+predictions = knn.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy of KNN (%): \n", accuracy)
