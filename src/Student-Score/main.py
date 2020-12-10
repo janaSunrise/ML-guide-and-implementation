096abad2b3efca16bf10da7f60b1e35878f5e521
Rohith04MVK <67266075+Rohith04MVK@users.noreply.github.com>
2020-12-10 11:58:07 +0530

    Made predictions, added linting

--- src/Student-Score/main.py
+++ src/Student-Score/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 from sklearn.linear_model import Lasso
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
@@ -32,4 +32,3 @@ plot_df.head(25).plot(kind='bar', figsize=(16, 10))
 plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
 plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 plt.show()
-
