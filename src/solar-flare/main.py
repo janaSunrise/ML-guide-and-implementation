1a015607840e1e0bb68aeb85deb00f398f56c9d1
Rohith04MVK <67266075+Rohith04MVK@users.noreply.github.com>
2020-12-12 14:48:32 +0530

    Update main.py

--- src/solar-flare/main.py
+++ src/solar-flare/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.multioutput import MultiOutputClassifier
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import SVC
 
 
