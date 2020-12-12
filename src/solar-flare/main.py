5c528260114a04ac1afcab4b09d034ff43c2bfa0
Rohith04MVK <007rohitjj04@gmail.com>
2020-12-12 09:25:33 +0530

    Added predictions for solar flare

--- src/solar-flare/main.py
+++ src/solar-flare/main.py
@@ -1,4 +1,11 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.multioutput import MultiOutputClassifier
+
+encoder = LabelEncoder()
 
 column_names = [
     "class", "largest_spot_size", "spot_distribution", "activity",
@@ -7,7 +14,22 @@ column_names = [
     "common_flare", "moderate_flare", "severe_flare"
 ]
 
-flare_1 = pd.read_csv("flare1.csv", sep=" ", names=column_names)
-flare_2 = pd.read_csv("flare2.csv", sep=" ", names=column_names)
+flare_1 = pd.read_csv("flare1.csv", sep=" ", names=column_names).apply(LabelEncoder().fit_transform)
+flare_2 = pd.read_csv("flare2.csv", sep=" ", names=column_names).apply(LabelEncoder().fit_transform)
 
 df = pd.concat([flare_1, flare_2])
+
+X = df.drop(["common_flare", "moderate_flare", "severe_flare"], axis=1)
+y = df[["common_flare", "moderate_flare", "severe_flare"]]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+clf = MultiOutputClassifier(SVC(kernel='linear'))
+
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+print(f"Accuracy: {clf.score(X_test, y_test)}")
+
