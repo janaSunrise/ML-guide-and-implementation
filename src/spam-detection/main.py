c978bad2e2a91b56b6322f6318f3a923fde66bd9
janaSunrise <warriordefenderz@gmail.com>
2020-12-13 14:06:23 +0530

    Complete the spam detection

---
+++ src/spam-detection/main.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+df = pd.read_csv("spam_or_not_spam.csv").dropna().drop_duplicates()
+
+X = df.drop("email", axis=1).values
+y = df["label"].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+# Create the model
+model = DecisionTreeClassifier()
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+# Print stats
+print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
+print(f"Report: {classification_report(y_test, y_pred)}")
