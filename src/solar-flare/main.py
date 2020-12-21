87629362e4fa3e06e7043cb54e2130d5c5524e17
janaSunrise <warriordefenderz@gmail.com>
2020-12-21 09:56:41 +0530

    Fix comments in solar flare file

--- src/solar-flare/main.py
+++ src/solar-flare/main.py
@@ -15,14 +15,18 @@ column_names = [
 flare_1 = pd.read_csv("flare1.csv", sep=" ", names=column_names).apply(LabelEncoder().fit_transform)
 flare_2 = pd.read_csv("flare2.csv", sep=" ", names=column_names).apply(LabelEncoder().fit_transform)
 
+# Clean the datasets
 df = pd.concat([flare_1, flare_2])
 
 X = df.drop(["common_flare", "moderate_flare", "severe_flare"], axis=1)
 y = df[["common_flare", "moderate_flare", "severe_flare"]]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create Multiclassifier model
 model = MultiOutputClassifier(SVC(kernel='linear'))
 
 model.fit(X_train, y_train)
 
+# Print stats
 print(f"Accuracy: {model.score(X_test, y_test)}")
