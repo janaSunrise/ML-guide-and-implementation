4b144c6c9d46c8894065e43690268370b0b6ac2d
Rohith04MVK <67266075+Rohith04MVK@users.noreply.github.com>
2021-01-02 19:04:37 +0530

    Update main.py

--- src/gender-classification/main.py
+++ src/gender-classification/main.py
@@ -5,9 +5,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 
-df = pd.read_csv('./Transformed Data Set - Sheet1.csv').apply(LabelEncoder().fit_transform)
+df = pd.read_csv("./Transformed Data Set - Sheet1.csv").apply(LabelEncoder().fit_transform)
 
-X = df.drop(['Gender'], axis=1)
+X = df.drop(["Gender"], axis=1)
 y = df.Gender
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -18,4 +18,4 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
 # Printing the stats
-print(f"Accurecy: {accuracy_score(y_pred, y_test)}")
+print(f"Accuracy: {accuracy_score(y_pred, y_test)}")
