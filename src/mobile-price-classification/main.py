ba10f3e42fb0b4da4349791e6817fc6c0431d74b
Rohith04MVK <67266075+Rohith04MVK@users.noreply.github.com>
2021-01-09 19:35:48 +0530

    Made predictions for mobile price classification
    Made predictions for mobile price classification with 95% accuracy.

--- src/mobile-price-classification/main.py
+++ src/mobile-price-classification/main.py
@@ -1,2 +1,30 @@
-import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+train_df = pd.read_csv("train.csv")
+
+
+X = train_df.drop(["price_range"], axis=1).values
+y = train_df["price_range"].values
+
+# Feature selection
+fs = SelectKBest(score_func=f_classif, k=10)
+X = fs.fit_transform(X, y)
+print(f"Shape of X = {X.shape}")
+
+# Spliting into train and test data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Defining the model
+model = KNeighborsClassifier(n_neighbors=9)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+# Printing the stats of the model
+print(f"Accuracy: {accuracy_score(y_pred, y_test)}")
