3c056cdd4d00b8dcce51c7e59678334881bed124
Sunrit Jana <warriordefenderz@gmail.com>
2023-01-03 21:36:27 +0530

    start working on random forest and migrating

---
+++ machine-learning/random-forest/africa-recession/setup.sh
@@ -0,0 +1,7 @@
+kaggle datasets download -d chirin/african-country-recession-dataset-2000-to-2017
+
+# unzip the dataset
+unzip african-country-recession-dataset-2000-to-2017.zip
+
+# remove the zip file
+rm african-country-recession-dataset-2000-to-2017.zip
